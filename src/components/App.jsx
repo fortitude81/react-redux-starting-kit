9ea96de8f5b9c1ad26a4cec6d6bfe3350d4b2e28
Emmanuel Morales <emorales0024@gmail.com>
2016-04-05 22:26:57 -0500

    Add example from Material Design for nice appearance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const App = () => <div>{`Welcome to the Starting Kit!`}</div>;
+const App = () => (
+  <div className="demo-card-wide mdl-card mdl-shadow--2dp">
+    <div className="mdl-card__title">
+      <h2 className="mdl-card__title-text">{`Welcome to the Starting Kit!`}</h2>
+    </div>
+    <div className="mdl-card__supporting-text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Mauris sagittis pellentesque lacus eleifend lacinia...
+    </div>
+  </div>
+);
 
 export default App;
