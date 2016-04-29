ecb417b7defc89d2cdccf419cc52695f1c4b4817
Emmanuel Morales <emorales0024@gmail.com>
2016-04-28 21:56:25 -0500

    Sass enhancements

--- src/components/StaticMiniDashboard.jsx
+++ src/components/StaticMiniDashboard.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import '../scss/simpleCard.scss';
 
 export default ({ welcomeMsg, cards }) => (
   <div className="mdl-layout__container">
     <div className="mdl-layout">
       <div className="mdl-layout mdl-color--grey-100">
-        <div className="simple-card-wide mdl-card mdl-shadow--2dp">
+        <div className="welcome-card">
           <div className="mdl-card__title"
             style={{background: 'url(' + welcomeMsg.imgUrl  + ') center/cover'}}>
             <h2 className="mdl-card__title-text">{welcomeMsg.title}</h2>
@@ -14,7 +13,7 @@ export default ({ welcomeMsg, cards }) => (
         </div>
         <div className="cards-wrapper">
           {cards.map((card, index) => (
-            <div key={index} className="simple-card-wide mdl-card mdl-shadow--2dp">
+            <div key={index} className="card-item">
               <div className="mdl-card__title"
                 style={{background: 'url(' + card.imgUrl  + ') center/cover'}}>
                 <h2 className="mdl-card__title-text">{card.title}</h2>
